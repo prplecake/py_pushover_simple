2dba65562cf6f4c48603c47465cc67b2a986a666
Matthew Jorgensen <matthew@jrgnsn.net>
2018-12-11 16:04:07 -0600

    Corrected project homepage and increased minor version to 0.2!

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py_pushover_simple",
-    version="0.1.4",
+    version="0.2",
     author="Matthew Jorgensen",
     author_email="matthew@jrgnsn.net",
     description="A wrapper for sending push notifications with Puhsover",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mtthwjrgnsn/py_pushover_simple",
+    url="https://git.sr.ht/~mjorgensen/py_pushover_simple",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
