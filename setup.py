7367ba22b9736226ee7c5333a2c446c2e30a45b1
Matthew Jorgensen <matthew@jrgnsn.net>
2018-12-11 16:38:57 -0600

    Added classifications and project URLs.
    These changes should populate on PyPi with 0.2.1.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    readme = fh.read()
 
 setuptools.setup(
     name="py_pushover_simple",
@@ -9,13 +9,26 @@ setuptools.setup(
     author="Matthew Jorgensen",
     author_email="matthew@jrgnsn.net",
     description="A wrapper for sending push notifications with Puhsover",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description=readme,
     url="https://git.sr.ht/~mjorgensen/py_pushover_simple",
+    project_urls={
+        "Documentation": "https://man.sr.ht/~mjorgensen/py_pushover_simple",
+        "Code": "https://git.sr.ht/~mjorgensen/py_pushover_simple",
+        "Issue Tracker": "https://todo.sr.ht/~mjorgensen/py_pushover_simple",
+    },
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Plugins",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Natural Language :: English",
+        "Topic :: Communications",
+        "Topic :: Utilities",
     ),
 )
