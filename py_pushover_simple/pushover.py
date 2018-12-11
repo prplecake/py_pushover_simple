9b2b006cb5911b1743b9457b70b1140fe9f8b65f
Matthew Jorgensen <matthew@jrgnsn.net>
2018-12-11 16:02:07 -0600

    Increased support for Pushover's API parameters.
    Support for the following API parameters has been implemented:
    
    * `title`
    * `sound`
    * `priority` (as well as `expire` and `retry` parameters required when
    `priority` is 2)
    * `timestamp`
    
    Pushover API Reference: https://pushover.net/api
    
    I also tweaked argparse and added a PushoverError Exception class.

--- py_pushover_simple/pushover.py
+++ py_pushover_simple/pushover.py
@@ -1,48 +1,75 @@
 import http.client
 import urllib
 import argparse
+import os.path
+import json
 
 
 def argParse():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-u', metavar='<string>', help='pushover user token')
-    parser.add_argument('-t', metavar='<string>', help='pushover app token')
+    parser.add_argument('-u', '--user_key', metavar='<string>', help='pushover user token')
+    parser.add_argument('-t', '--app_token', metavar='<string>', help='pushover app token')
 
     return parser.parse_args()
 
 
+class PushoverError(Exception): pass
+
+
 class Pushover:
-    def __init__(self, user=None, token=None,
-                 target=None, url=None, url_title=None):
+    def __init__(self, user=None, token=None, sound=None,
+                 target=None, url=None, url_title=None,
+                 title=None, priority=0, timestamp=None,
+                 retry=None, expire=None,):
         self.url = url
         self.user = user
+        self.sound = sound
+        self.title = title
         self.token = token
         self.target = target
+        self.priority = priority
+        self.timestamp = timestamp
         self.url_title = url_title
+        self.retry = retry
+        self.expire = expire
 
     def sendMessage(self, message):
         conn = http.client.HTTPSConnection("api.pushover.net:443")
         conn.request("POST", "/1/messages.json",
                      urllib.parse.urlencode({
-                        "token": self.token,
-                        "user": self.user,
                         "url": self.url,
-                        "url_title": self.url_title,
+                        "user": self.user,
+                        "sound": self.sound,
+                        "title": self.title,
+                        "token": self.token,
                         "device": self.target,
                         "message": message,
+                        "priority": self.priority,
+                        "timestamp": self.timestamp,
+                        "url_title": self.url_title,
+                        "retry": self.retry,
+                        "expire": self.expire,
                         }),
                      {"Content-type": "application/x-www-form-urlencoded"})
-        conn.getresponse()
-        if __name__ == '__main__':
-            return conn.getresponse()
+        output = conn.getresponse().read().decode('utf-8')
+        data = json.loads(output)
+
+        if data['status'] != 1:
+            raise PushoverError(output)
+        else:
+            return True
+
+        self.data = data
+
+        return self
 
 
 def main():
     args = argParse()
 
     userKeySet = args.user_key or args.user_key == ""
-    appTokenSet = args.token or args.token == ""
+    appTokenSet = args.app_token or args.app_token == ""
 
     print(userKeySet)
     print(appTokenSet)
@@ -54,10 +81,13 @@ def main():
         exit()
     else:
         user = args.user_key
-        token = args.token
+        token = args.app_token
 
-    p = Pushover(user, token)
-    p.sendMessage("Testing pushover.py")
+    p = Pushover()
+    p.user = user
+    p.token = token
+    p.title = os.path.basename(__file__)
+    p.sendMessage("Testing pushover.py...")
 
 
 if __name__ == '__main__':
